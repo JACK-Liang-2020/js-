01d2c4982cadb67acab1b16f0eea65673d218ed0
liangqiujie <708985420@qq.com>
2020-04-03 16:21:47 +0800

    优化代码

--- bubbleSort.js
+++ bubbleSort.js
@@ -28,9 +28,11 @@ function bubbleSort(arr) {
     for (var j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         // 相邻元素两两对比
-        var temp = arr[j + 1]; // 元素交换
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+        // var temp = arr[j + 1]; // 元素交换
+        // arr[j + 1] = arr[j];
+        // arr[j] = temp;
+
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
--- selectionSort.js
+++ selectionSort.js
@@ -11,24 +11,35 @@
 */
 
 /* 
-再解析：
-
+再解析：假设len等于8
+  1.len-1为7，i在执行最后一次循环的时候为6
+  2.j为i+1，j在循环中最小值为1，最大值为8，但是最终j不参与循环的时候，等于9
+  3.排序的方向是找到最小的数，存放到起始位置
 */
 
 function selectionSort(arr) {
   var len = arr.length;
   var minIndex, temp;
+
+  // 为什么len-1?
   for (var i = 0; i < len - 1; i++) {
     minIndex = i;
+    // 为什么i+1?
     for (var j = i + 1; j < len; j++) {
       if (arr[j] < arr[minIndex]) {
         // 寻找最小的数
-        minIndex = j; // 将最小数的索引保存
+        // 将最小数的索引保存
+        minIndex = j;
       }
     }
-    temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
+
+    // temp = arr[i];
+    // arr[i] = arr[minIndex];
+    // arr[minIndex] = temp;
+
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
   }
   return arr;
 }
+
+selectionSort([1, 5, 3, 6, 77, 6, 888, 765, 354, 34]);
